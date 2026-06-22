2913d05977d3cc0f32369e050613f23a6a27df8e
Lucas <lucasryutaka@gmail.com>
2026-06-22 17:16:49 -0300

    initial commit

--- src/silver_pipeline.py
+++ src/silver_pipeline.py
@@ -1,6 +1,7 @@
-import shutil
 import logging
 import json
+import re
+from datetime import datetime 
 from pathlib import Path
 
 logging.basicConfig(
@@ -14,8 +15,46 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-p = Path('medallion/bronze')
 
-files = list(p.glob('*'))
+def limpeza(data: dict): 
+    tratado = {
+        'id': data['id'],
+        'data': data['date'][:10],
+        'id_cliente': data['client_id'],
+        'recipe':
+            [{
+                'marca_comercial': i['marca_comercial'].capitalize(),
+                'ingrediente_ativo': i['ingrediente_ativo'].capitalize(),
+                'formulacao': i['formulacao'].strip(),
+                'tecnica': i['tecnica'].capitalize(),
+                'cultura': i['cultura'].capitalize(),
+                'praga': re.sub(r'\s+', ' ',i['praga'].strip().replace('-', ' ')).capitalize(),
+                'dosagem': i['dosagem']
+            } for i in data['recipe']
+            ]
+    }
+    return tratado
 
-print(files[0])
+
+def ler_bronze(path):
+    data = []
+    p = Path(str(path))
+    files = [f for f in p.glob('*') if f.name != '.gitkeep']
+    for i in files:
+        with open(i, 'r', encoding='utf-8') as file:
+            json_data = json.load(file)
+            data_clean = limpeza(json_data)
+            data.append(data_clean)
+    return data
+
+def salvar_silver(path, data):
+    p = Path(path)
+    p.mkdir(parents=True, exist_ok=True)
+
+    file_path = p / f'{datetime.today().strftime('%Y-%m-%d')}.json'
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+
+data = ler_bronze('medallion/bronze')
+
+salvar_silver('medallion/silver', data)
